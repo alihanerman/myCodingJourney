db7adf5af55687a04a3db32b74c1c55e5072bca9
alihanErman <alihanerman1@gmail.com>
2023-04-26 18:07:14 +0300

    ortalama hesaplayıcı son

--- NotOrtalamasiHesaplayici/src/p2/Main.java
+++ NotOrtalamasiHesaplayici/src/p2/Main.java
@@ -9,16 +9,22 @@ public class Main {
         int  mat , fizik ,kimya , turkce, tarih ,muzik ;
 
         Scanner scan = new  Scanner (System.in);
+        System.out.println("Mat: ");
  mat = scan.nextInt();
+        System.out.println("Fizik: ");
  fizik = scan.nextInt();
+        System.out.println("Kimya: ");
  kimya = scan.nextInt();
+        System.out.println("Turkçe:  ");
  turkce = scan.nextInt();
+        System.out.println("Tarih:  ");
  tarih = scan.nextInt();
+        System.out.println("Muzik:  ");
  muzik = scan.nextInt();
         scan.close();
  int result = (mat + fizik + kimya+ turkce + tarih +muzik)/6;
-        System.out.println("mat= " +mat + " fizik=" +fizik+ " kimya= " +kimya+ " turkce= " +turkce+ " tarih= " + tarih+ " müzik= "+muzik );
-        System.out.println("ortalama" +result);
+        System.out.println("mat= " +mat + " fizik= " +fizik+ " kimya= " +kimya+ " turkce= " +turkce+ " tarih= " + tarih+ " müzik= "+muzik );
+        System.out.println("ortalama: " +result);
 
         if (result < 60 && result >= 0) {
             System.out.println("Kaldınız");}
