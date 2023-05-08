f9991639a32baea82c52e43e274cc0c9eba29fa9
alihanErman <alihanerman1@gmail.com>
2023-05-08 22:23:58 +0300

    for loop exercise

---
+++ ReverseTriangle/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter a number
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        // Use nested loops to print the reverse triangle
+        for (int i = num; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
